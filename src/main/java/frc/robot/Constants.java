ad98063fa864dfc918410c68270ffc03bb6d6f3d
Harris N <54484642+harrislegobirck@users.noreply.github.com>
2020-01-05 12:30:11 -0500

    Added limelight constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,22 +8,31 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    public final class Drive{
+    public final class Drive {
         public static final int leftMaster = 0;
         public static final int leftSlave = 1;
         public static final int rightMaster = 2;
         public static final int rightSlave = 3;
     }
-    public final class JoySticks{
+
+    public final class JoySticks {
         public static final int leftStick = 0;
         public static final int rightStick = 1;
+    }
+
+    public final class Limelight {
+        public static final double a1 = 0; // mounting angle
+        public static final double h1 = 2; // height of your camera above the floor (in feet)
+        public static final double h2 = 6.9375; // height of the target (in feet)
     }
 }
