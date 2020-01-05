fffda6c5de0f64f655c09ac117ffd6769760fe2c
Harris N <54484642+harrislegobirck@users.noreply.github.com>
2020-01-05 11:42:11 -0500

    drivebase

---
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -0,0 +1,51 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+
+public class TankDrive extends CommandBase {
+  private final Drivetrain driveSubsystem;
+  private final Joystick left, right;
+
+  /**
+   * Creates a new TankDrive.
+   */
+  public TankDrive(Drivetrain driveSubsystem, Joystick left, Joystick right) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.driveSubsystem = driveSubsystem;
+    this.left = left;
+    this.right = right;
+    addRequirements(this.driveSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    driveSubsystem.drive(left.getY(), right.getY());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    driveSubsystem.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
