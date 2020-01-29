0193fbf75e5b8e189d67ea3f26ddf07fa4d50f84
Harris N <harrislegobrick@gmail.com>
2020-01-29 15:29:44 -0500

    Added pneumatics to this class
    why did i seperate it before

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -11,21 +11,35 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
   private final VictorSPX motor;
+  private final DoubleSolenoid intake;
 
   /**
    * Creates a new Intake.
    */
   public Intake() {
     motor = new VictorSPX(Constants.Intake.MOTOR);
+    intake = new DoubleSolenoid(Constants.Pneumatics.INTAKE_FORWARD, Constants.Pneumatics.INTAKE_REVERSE);
+
+    intake.set(Value.kReverse);
 
     motor.configFactoryDefault();
     motor.setInverted(Constants.Intake.INVERTED);
     motor.setNeutralMode(NeutralMode.Brake);
+  }
+
+  public void deployIntake() {
+    intake.set(Value.kForward);
+  }
+
+  public void retractIntake() {
+    intake.set(Value.kReverse);
   }
 
   public void run() {
