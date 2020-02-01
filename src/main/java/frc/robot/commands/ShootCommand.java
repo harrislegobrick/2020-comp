7578a76b7fbfdf3280bace68631ca9cb7fc09854
Harris N <54484642+harrislegobirck@users.noreply.github.com>
2020-02-01 01:30:10 -0500

    Added limelight distance into velocity calculation

--- src/main/java/frc/robot/commands/ShootCommand.java
+++ src/main/java/frc/robot/commands/ShootCommand.java
@@ -9,15 +9,19 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Flywheel;
+import frc.robot.subsystems.Limelight;
 
 public class ShootCommand extends CommandBase {
   private final Flywheel flywheel;
+  private final Limelight limelight;
+
   /**
    * Creates a new ShootCommand.
    */
-  public ShootCommand(Flywheel flywheel) {
+  public ShootCommand(Flywheel flywheel, Limelight limelight) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.flywheel = flywheel;
+    this.limelight = limelight;
     addRequirements(flywheel);
   }
 
@@ -29,7 +33,11 @@ public class ShootCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    flywheel.setVelocity(2000);
+    // 4000 / 15 = 267 so multiply distance by 267 to get RPM?
+    if (limelight.getDistance() > 5)
+      flywheel.setVelocity(267 * limelight.getDistance());
+    else
+      flywheel.setVelocity(4000);
   }
 
   // Called once the command ends or is interrupted.
