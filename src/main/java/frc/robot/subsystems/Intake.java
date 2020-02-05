fa5e09d886127e05cfe5617caca78d28f294d293
Harris N <harrislegobrick@gmail.com>
2020-02-05 17:25:12 -0500

    Increased speed

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -43,7 +43,7 @@ public class Intake extends SubsystemBase {
   }
 
   public void run() {
-    motor.set(ControlMode.PercentOutput, 0.3);
+    motor.set(ControlMode.PercentOutput, 0.35);
   }
 
   public void stop() {
